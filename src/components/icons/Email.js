2c7c09acc96916025cf61b78f05db0878b3662ff
Mohit <mototurbo12499@gmail.com>
2023-01-12 00:39:52 +0530

    Removed the SSR , switched back to SSG.

--- src/components/icons/Email.js
+++ src/components/icons/Email.js
@@ -3,19 +3,16 @@ import React from 'react';
 const IconEmail = ({ name }) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 64 64"
-        aria-labelledby="title"
-        aria-describedby="desc"
-        role="img"
+        // width="24" height="24"
+        viewBox="0 0 24 24"
         fill="none"
-        strokeWidth="3.5"
         stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className={name}>
-        <title>Mail</title>
-        <path d="M2 12l30 29 30-29M42 31.6L62 52M2 52l20-20.4" />
-        <path d="M2 12h60v40H2z" />
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class={`feather feather-mail icon ${name}`}>
+        <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
+        <polyline points="22,6 12,13 2,6" />
     </svg>
 )
 export default IconEmail;
