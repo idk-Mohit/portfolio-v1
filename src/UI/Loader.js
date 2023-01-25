fdec6eea9f764335f8d219e6f8cde1d099a11077
Mohit <mototurbo12499@gmail.com>
2023-01-25 15:44:47 +0530

    removed unwanted library

--- src/UI/Loader.js
+++ src/UI/Loader.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import LoaderIcon from './loadericon.js'
 import { loaderDelay } from '../utils/config.js'
