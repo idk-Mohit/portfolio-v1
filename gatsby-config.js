2083df0aac1a97edbecdab76d1aba84a8f103aa6
Mohit <30345679+idk-Mohit@users.noreply.github.com>
2024-05-29 18:41:12 +0530

    .

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,24 +3,33 @@ module.exports = {
     title: `idk-mohit`,
     description: `Hello, My name is Mohit. I’m a Web Developer specializing in building fast, attractive digital experiences. Currently, I’m focused on building Personal and freelance projects. And Available for opportunities.`,
     siteUrl: `https://idk-mohit.vercel.app`,
-    image: 'favicon-32x32.png',
+    image: "favicon-32x32.png",
   },
   flags: {
-    DEV_SSR: true
+    DEV_SSR: true,
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", `gatsby-plugin-scroll-reveal`, {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png",
-      start_url: '/',
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-      resolve: 'gatsby-source-filesystem',
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    `gatsby-plugin-scroll-reveal`,
+    {
+      resolve: "gatsby-plugin-manifest",
       options: {
-        "name": "images",
-        "path": "./src/images/"
+        icon: "src/images/icon.png",
+        start_url: "/",
       },
-      __key: "images"
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
 
     {
@@ -31,20 +40,22 @@ module.exports = {
             resolve: `gatsby-remark-images`,
           },
           `gatsby-remark-lazy-load`,
-        ]
-      }
+        ],
+      },
     },
     {
-      resolve: 'gatsby-plugin-html-attributes',
+      resolve: "gatsby-plugin-html-attributes",
       options: {
-        lang: 'en'
-      }
-    }, {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        "name": "pages",
-        "path": "./src/pages/"
+        lang: "en",
       },
-      __key: "pages"
-    }],
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
 };
